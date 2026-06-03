e1706aa70a8b7039e6aa7ed6e5588c6c77d86e68
its lux the luxy <lux@the-luxy.lux>
2026-06-03 22:08:14 +0700

    fix: trigger fresh deployment for env vars

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,11 +12,23 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    public function boot(): void
-    {
-        // Jika diakses lewat domain ngrok, paksa semua link & form memakai HTTPS
-        if (str_contains(request()->getHost(), 'ngrok-free.dev')) {
-            URL::forceScheme('https');
+    public function boot()
+{
+    // Tambahkan baris kode ini untuk memotong jalur penulisan folder lokal ke folder sementara Vercel
+    if (config('app.env') === 'production') {
+        $paths = [
+            '/tmp/framework/sessions',
+            '/tmp/framework/cache',
+            '/tmp/framework/views',
+            '/tmp/storage/bootstrap/cache',
+            '/tmp/storage/framework/cache',
+        ];
+        foreach ($paths as $path) {
+            if (!is_dir($path)) {
+                mkdir($path, 0755, true);
+            }
         }
     }
+}
+
 }
