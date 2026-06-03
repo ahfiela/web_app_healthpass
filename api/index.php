af7ed88dfa79ed53cac03d08899e066e205a9569
its lux the luxy <lux@the-luxy.lux>
2026-06-03 22:00:41 +0700

    nambahin perubahan vercel.json commit

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
 <?php
+
+// 1. Alihkan folder storage dan cache ke /tmp agar tidak Error 500 di Vercel
+$storagePath = '/tmp/storage/bootstrap/cache';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0755, true);
+}
+
+putenv("APP_STORAGE=/tmp");
+putenv("VIEW_COMPILED_PATH=/tmp/storage/bootstrap/cache");
+
+// 2. Hubungkan kembali ke file utama Laravel Anda
 require __DIR__ . '/../public/index.php';
